a7b1fe9ee243f37be68599a8218ddd40bd7cf295
Mehdi Kurdamir <69827292+mehdikurdamir@users.noreply.github.com>
2021-02-21 20:17:54 +0330

    Update code.py

--- code.py
+++ code.py
@@ -1,11 +1,13 @@
 s = int(input("Enter the start of range: "))
 e = int(input("Enter the end of range: "))
+
 print("All even numbers are :")
 for i in range(s , e + 1):
     if i % 2 == 0:
-        print(i)        
+        print(f'{i:2}' , end=' ')
+print(' ')                
 
 print("All odd numbers are :")
 for i in range(s , e + 1):
     if i % 2 == 1:
-        print(i)
+        print(f'{i:2}' , end=' ')
